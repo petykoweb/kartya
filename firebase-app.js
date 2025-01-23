e4b3d68ae50f8824699637cce97d85d52091a7ce
petykoweb <nubsos123@gmail.com>
2025-01-23 21:57:08 +0100

    Update firebase-app.js

--- firebase-app.js
+++ firebase-app.js
@@ -1,18 +1,20 @@
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+<script type="module">
+  // Import the functions you need from the SDKs you need
+  import { initializeApp } from "https://www.gstatic.com/firebasejs/11.2.0/firebase-app.js";
+  // TODO: Add SDKs for Firebase products that you want to use
+  // https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your web app's Firebase configuration
-const firebaseConfig = {
-  apiKey: "AIzaSyCuGJhygh3Jg4ThLlKIsJrQhhwdGZTPqcE",
-  authDomain: "kartyashop-90596.firebaseapp.com",
-  databaseURL: "https://kartyashop-90596-default-rtdb.firebaseio.com",
-  projectId: "kartyashop-90596",
-  storageBucket: "kartyashop-90596.firebasestorage.app",
-  messagingSenderId: "891615268581",
-  appId: "1:891615268581:web:46bd572eb2b12ad3820e0c"
-};
+  // Your web app's Firebase configuration
+  const firebaseConfig = {
+    apiKey: "AIzaSyCuGJhygh3Jg4ThLlKIsJrQhhwdGZTPqcE",
+    authDomain: "kartyashop-90596.firebaseapp.com",
+    databaseURL: "https://kartyashop-90596-default-rtdb.firebaseio.com",
+    projectId: "kartyashop-90596",
+    storageBucket: "kartyashop-90596.firebasestorage.app",
+    messagingSenderId: "891615268581",
+    appId: "1:891615268581:web:46bd572eb2b12ad3820e0c"
+  };
 
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
+</script>
